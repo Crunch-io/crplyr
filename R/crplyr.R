8ddb13a3ab12044d85be113a0fdba16a682f8d6d
Neal Richardson <neal@crunch.io>
2019-01-11 14:58:43 -0800

    Some prep for release

--- R/crplyr.R
+++ R/crplyr.R
@@ -1,10 +1,11 @@
 # Here's a good place to put your top-level package documentation
 
+#' @importFrom crunch set_crunch_config
 .onLoad <- function (lib, pkgname="crplyr") {
     # Re-set the API config so that crplyr shows up in the user-agent string
-    crunch::set_crunch_config(update=TRUE)
+    set_crunch_config(update=TRUE)
     return(invisible())
 }
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+if (getRversion() >= "2.15.1") utils::globalVariables(".")
