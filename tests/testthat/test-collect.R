aeb7c7eb6a9178ed7147fdc858ad2387fe788fae
Neal Richardson <neal@crunch.io>
2019-03-27 08:29:40 -0700

    Allow select() to grab hidden variables; test that in collect()

--- tests/testthat/test-collect.R
+++ tests/testthat/test-collect.R
@@ -10,14 +10,24 @@ with_mock_crunch({
     ds <- loadDataset("test ds")
     test_that("collect generates expected POST request", {
         expect_POST(
-            ds %>% 
-                select(gender, birthyr) %>% 
+            ds %>%
+                select(gender, birthyr) %>%
                 collect(),
             'https://app.crunch.io/api/datasets/1/export/csv/',
             '{"filter":null,"where":{"function":"select","args":[{"map":{"66ae9881e3524f7db84970d556c34552":{"variable":"https://app.crunch.io/api/datasets/1/variables/gender/"},"f78ca47313144b57adfb495893968e70":{"variable":"https://app.crunch.io/api/datasets/1/variables/birthyr/"}}}]},"options":{"use_category_ids":true}}'
         )
     })
-    
+    ds3 <- loadDataset("ECON.sav")
+    test_that("collect can pull hidden variables", {
+        expect_POST(
+            ds3 %>%
+                select(gender, birthyr) %>%
+                collect(),
+            'https://app.crunch.io/api/datasets/3/export/csv/',
+            '{"filter":null,"where":{"function":"select","args":[{"map":{"66ae9881e3524f7db84970d556c34552":{"variable":"https://app.crunch.io/api/datasets/3/variables/gender/"},"f78ca47313144b57adfb495893968e70":{"variable":"https://app.crunch.io/api/datasets/3/variables/birthyr/"}}}]},"options":{"use_category_ids":true}}'
+        )
+    })
+
     test_that("collect doesn't affect dplyr methods", {
         expect_identical(mtcars, collect(mtcars))
     })
