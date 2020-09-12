90ecb14c1a62cf12f1d54024568cd478499806d8
Greg Freedman Ellis <greg@crunch.io>
2020-09-12 14:29:46 -0500

    Can now use formulas in arguments

--- R/mutate-automation.R
+++ R/mutate-automation.R
@@ -2,22 +2,65 @@
 as_crunch_auto_tbl <- function(x) { # TODO: move to a S4 initializer?
   out <- AutoReadyCrunchDataset(x)
   out@steps <- list()
-  out@var_tibble <- crunch_var_df(x)
+  out@var_tibble <- as_crunch_var_df(x)
   out
 }
 
 # A data.frame with list columns containing crunch variables
 # `tibble` doesn't allow this data.frame because CrunchVars
 # are not true "vectors" so be careful of manipulating this
-crunch_var_df <- function(dataset) {
-  all_aliases <- aliases(allVariables(dataset))
-  var_list <- lapply(all_aliases, function(alias) dataset[[alias]])
-  names(var_list) <- all_aliases
-  structure(var_list, row.names = c(NA, -1L), class = "data.frame")
+#' @export
+crunch_var_df <- setClass("crunch_var_df", contains = "data.frame")
+
+#' @importFrom purrr map map_chr
+as_crunch_var_df <- function(vars) { #TODO: use traditional S4 "initialize"?
+  if (is.dataset(vars)) {
+    all_aliases <- aliases(allVariables(vars))
+    var_list <- map(all_aliases, function(alias) vars[[alias]])
+    names(var_list) <- all_aliases
+  } else {
+    # across gives a data.frame 
+    # TODO: this may already be what we want, or at least might be with correct subsetting
+    # defined on the crunch_var_df class, but need to check
+    if (is.data.frame(vars[[1]])) { 
+      vars <- purrr::flatten(vars) 
+    }
+    all_aliases <- map_chr(vars, function(var) if (is.variable(var)) alias(var) else as.character(var))
+    var_list <- setNames(vars, all_aliases)
+  }
+  
+  var_df <- structure(var_list, row.names = c(NA, -1L), class = c("data.frame"))
+  crunch_var_df(var_df)
 }
 
+#' @export
+setMethod("aliases", "crunch_var_df", function(x) {
+  names(x)
+})
+
+#' @export
+setMethod("titles", "crunch_var_df", function(x) {
+  if (!all(map_lgl(x, is.variable))) stop("All variables must be CrunchVariables")
+  map_chr(x, name)
+})
+
+#' @export
+setMethod("descriptions", "crunch_var_df", function(x) {
+  if (!all(map_lgl(x, is.variable))) stop("All variables must be CrunchVariables")
+  map_chr(x, description)
+})
+
+#' @export
+setMethod("notes", "crunch_var_df", function(x) {
+  if (!all(map_lgl(x, is.variable))) stop("All variables must be CrunchVariables")
+  map_chr(x, notes)
+})
+
+
 calculate_steps <- function(var_df, ...) {
-  steps <- transmute(var_df, ...)
+  # It seems silly to remove class here, but dplyr doesn't like operating
+  # on the S4 class, possibly could fix by defining vcts attributes correctly
+  steps <- transmute(data.frame(var_df), ...)
   
   lapply(names(steps), function(step_alias) {
     out <- steps[[step_alias]][[1]]
@@ -66,7 +109,13 @@ categorical_array <- function(
   description = NULL, 
   notes = NULL
 ) {
-  sv_aliases <- ca_aliases(list(...))
+  .vars <- as_crunch_var_df(list(...))
+  sv_aliases <- map(aliases(.vars), noquote)
+  
+  labels <- ca_process_formula(labels, .vars)
+  title <- ca_process_formula(title, .vars)
+  description <- ca_process_formula(description, .vars)
+  notes <- ca_process_formula(notes, .vars)
   
   cmd <- crunch_auto_cmd(
     ca_template(
@@ -97,14 +146,20 @@ convert_to_text <- function(
   description = NULL,
   notes = NULL
 ) {
-  old_aliases <- ca_aliases(list(...))
+  .vars <- as_crunch_var_df(list(...))
+  old_aliases <- map(aliases(.vars), noquote)
+  
+  new_aliases <- ca_process_formula(new_aliases, .vars)
+  title <- ca_process_formula(title, .vars)
+  description <- ca_process_formula(description, .vars)
+  notes <- ca_process_formula(notes, .vars)
 
   cmd <- crunch_auto_cmd(
     function(x) {
       if (is.null(x$new_aliases)) {
         x$new_aliases <- if (length(x$old_aliases) == 1) list(noquote(x$alias)) else x$old_aliases
       } else {
-        x$new_aliases <- lapply(x$new_aliases, noquote)
+        x$new_aliases <- map(x$new_aliases, noquote)
       }
       
       if (identical(x$old_aliases, x$new_aliases)) {
@@ -127,8 +182,8 @@ convert_to_text <- function(
     },
     old_aliases = old_aliases,
     new_aliases = new_aliases,
-    titles = titles,
-    descriptions = descriptions,
+    title = title,
+    description = description,
     notes = notes
   )
   
@@ -145,11 +200,12 @@ format_ca_auto <- function(x, ...) {
   x$formatter(x$data)
 }
 
-ca_aliases <- function(vars) {
-  if (is.data.frame(vars[[1]])) { # across gives a data.frame
-    vars <- purrr::flatten(vars) 
-  }
-  lapply(vars, function(x) if (is.variable(x)) noquote(alias(x)) else x)
+ca_process_formula <- function(x, data) {
+  if (rlang::is_function(x) || rlang::is_formula(x)) {
+    f <- rlang::as_function(x)
+    x <- f(data)
+  } 
+  x
 }
 
 ca_comma_separated <- function(items, newline = FALSE, indent = 0) {
