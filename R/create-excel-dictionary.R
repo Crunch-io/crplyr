70c83a9fe30d66adef81d8ff8e61e37867bfe715
Greg Freedman Ellis <greg@crunch.io>
2020-12-01 09:48:58 -0600

    writing excel dictionary uses new format

--- R/create-excel-dictionary.R
+++ R/create-excel-dictionary.R
@@ -120,16 +120,9 @@ build_subvars <- function(ds, vars_with_subvars) {
                 orig_alias = alias,
                 orig_child_alias = crunch::aliases(subvars),
                 child_alias = crunch::aliases(subvars),
-                title = names(subvars),
-                description = crunch::descriptions(subvars),
-                notes = crunch::notes(subvars)
+                subvar_label = names(subvars)
             )
         }
-    )
-    # users of excel won't be able to tell the difference between "" and NA so conver to NA
-    subvars <- dplyr::mutate(
-        subvars,
-        dplyr::across(dplyr::one_of(c("description", "notes")), ~ifelse(. == "", NA, .))
     )
     subvars <- dplyr::nest_by(subvars, .data$orig_alias, .key = "subvars")
     subvars
@@ -161,24 +154,33 @@ save_excel_dictionary <- function(data, out_file, overwrite = FALSE) {
     var_info <- dplyr::mutate(data, var_order = dplyr::row_number())
 
     subvars <- dplyr::filter(var_info, purrr::map_lgl(.data$subvars, ~!is.null(.) && nrow(.) > 0))
-    subvars <- dplyr::select(subvars, dplyr::one_of(c("var_order", "orig_alias", "subvars")))
+    subvars <- dplyr::select(
+        subvars,
+        c("var_order", "folder", "alias", "title", "description", "notes", "orig_alias", "subvars")
+    )
     subvars <- tidyr::unnest(subvars, .data$subvars)
+    subvars <- dplyr::group_by(subvars, .data$var_order)
+    subvars <- dplyr::mutate(
+        subvars,
+        dplyr::across(c("folder", "title", "description", "notes"), ~ifelse(dplyr::row_number() == 1, ., NA))
+    )
     subvars <- dplyr::mutate(subvars, sv_order = dplyr::row_number(), cat_order = -Inf)
 
     categories <- dplyr::filter(var_info, purrr::map_lgl(.data$categories, ~!is.null(.) && nrow(.) > 0))
-    categories <- dplyr::select(categories, dplyr::one_of(c("var_order", "orig_alias", "categories")))
+    categories <- dplyr::select(categories, c("var_order", "orig_alias", "categories"))
     categories <- tidyr::unnest(categories, .data$categories)
     categories <- dplyr::mutate(categories, sv_order = Inf, cat_order = dplyr::row_number())
 
-    var_info <- dplyr::select(var_info, -dplyr::one_of(c("subvars", "categories")))
+    var_info <- dplyr::filter(var_info, purrr::map_lgl(.data$subvars, ~is.null(.) || nrow(.) == 0))
+    var_info <- dplyr::select(var_info, -c("subvars", "categories"))
     var_info <- dplyr::mutate(var_info, sv_order = -Inf, cat_order = -Inf)
 
     out <- dplyr::bind_rows(var_info, subvars, categories)
     out <- dplyr::arrange(out, .data$var_order, .data$sv_order, .data$cat_order)
     out <- dplyr::select(out, dplyr::one_of(c(
         "type", "folder", "orig_alias", "orig_child_alias", "alias", "child_alias", "title",
-        "description", "notes", "orig_code", "code", "name", "missing", "selected", "value",
-        "date"
+        "subvar_label", "description", "notes", "orig_code", "code", "name", "missing", "selected",
+        "value", "date"
     )))
 
     out <- dplyr::mutate(out, dplyr::across(c("missing", "selected"), ~ifelse(., "X", NA)))
@@ -186,32 +188,64 @@ save_excel_dictionary <- function(data, out_file, overwrite = FALSE) {
 
     wb <- openxlsx::createWorkbook(creator = "crunch")
     ws <- openxlsx::addWorksheet(wb, "Variables Editor")
-    openxlsx::setColWidths(wb, ws, 1:16, c(16, 30, 10, 10, 3, 30, 25, 25, 25, 7.5, 10, 25, 7.5, 7.5, 7.5, 7.5))
-
-    openxlsx::mergeCells(wb, ws, 10:16, 1)
-    openxlsx::writeData(wb, ws, "Categories", startCol = 10, startRow = 1)
+    openxlsx::setColWidths(wb, ws, 1:17, c(16, 30, 10, 10, 10, 10, 25, 25, 25, 25, 7.5, 10, 25, 7.5, 7.5, 7.5, 7.5))
+    openxlsx::setColWidths(wb, ws, 18, hidden = TRUE)
+    openxlsx::mergeCells(wb, ws, 11:17, 1)
+    openxlsx::writeData(wb, ws, "Categories", startCol = 11, startRow = 1)
     openxlsx::writeData(
         wb,
         ws,
-        as.data.frame(matrix(nrow = 1, ncol = 16, c(
-            "Type", "Folder", "Original Alias", "Original Child Alias", "Alias", NA, "Title", "Description", "Notes",
-            "Original Code", "Code", "Name", "Missing", "Selected", "Value", "Date"
+        as.data.frame(matrix(nrow = 1, ncol = 17, c(
+            "Type", "Folder", "Original Alias", "Original Child Alias", "Alias", "Child Alias", "Title",
+            "Subvariable Label", "Description", "Notes", "Original Code", "Code", "Name", "Missing",
+            "Selected", "Value", "Date"
         ))),
         startCol = 1,
         startRow = 2,
         colNames = FALSE
     )
 
-    header_style <- openxlsx::createStyle(textDecoration = "bold")
-    header_ul_style <- openxlsx::createStyle( border = "bottom", halign = "center")
-    dont_edit_style <- openxlsx::createStyle(fontColour = "#CCCCCC")
-    openxlsx::addStyle(wb, ws, header_ul_style, 1, 10:16)
-    openxlsx::addStyle(wb, ws, header_style, 1, 1:16, stack = TRUE)
-    openxlsx::addStyle(wb, ws, header_style, 2, 1:16, stack = TRUE)
-    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 2), 3, stack = TRUE)
-    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 2), 4, stack = TRUE)
-    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 2), 10, stack = TRUE)
-    openxlsx::freezePane(wb, ws, firstActiveRow = 3)
     openxlsx::writeData(wb, ws, out, startRow = 3, colNames = FALSE)
+
+    openxlsx::writeData(wb, ws, "Calc Alias", startCol = 17 + 1, startRow = 2)
+    openxlsx::writeFormula(
+        wb,
+        ws,
+        glue::glue(
+            "=IF({MAIN_COL}{ROW}<>\"\",{MAIN_COL}{ROW},OFFSET({CALC_COL}{ROW},-1,0))",
+            MAIN_COL = "E",
+            CALC_COL = "R",
+            ROW = 3:(nrow(out) + 202)
+        ),
+        startCol = 17 + 1,
+        startRow = 3
+    )
+    header_style <- openxlsx::createStyle(textDecoration = "bold")
+    header_ul_style <- openxlsx::createStyle(border = "bottom", halign = "center")
+    dont_edit_style <- openxlsx::createStyle(fontColour = "#CCCCCC")
+    beginning_of_var_style <- openxlsx::createStyle(border = "top", borderColour = "#888888")
+    openxlsx::conditionalFormatting(
+        wb,
+        ws,
+        1:17,
+        3:(nrow(out) + 202),
+        glue::glue("${CALC_COL}3<>OFFSET(${CALC_COL}3,-1,0)", CALC_COL = "R"),
+        beginning_of_var_style
+    )
+    # work around openxlsx limitation that you can't stack conditional formatting on top of other
+    # formattings, so munge the conditional formatting style
+    wb$styles$dxfs[1] <- paste0(
+        "<dxf><font><sz val=\"11\"/></font><border><top style=\"thin\">",
+        "<color rgb=\"FF888888\"/></top></border></dxf>"
+    )
+    openxlsx::addStyle(wb, ws, header_ul_style, 1, 11:17, stack = TRUE)
+    openxlsx::addStyle(wb, ws, header_style, 1, 1:18, stack = TRUE)
+    openxlsx::addStyle(wb, ws, header_style, 2, 1:18, stack = TRUE)
+    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 202), 3, stack = TRUE)
+    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 202), 4, stack = TRUE)
+    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 202), 11, stack = TRUE)
+    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 202), 11, stack = TRUE)
+    openxlsx::addStyle(wb, ws, dont_edit_style, 2:(nrow(out) + 202), 18, stack = TRUE)
+    openxlsx::freezePane(wb, ws, firstActiveRow = 3)
     openxlsx::saveWorkbook(wb, out_file, overwrite = overwrite)
 }
