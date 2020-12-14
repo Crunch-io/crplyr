7d174f07e1e1c2d21746343b42bcb24464d0d4f0
Greg Freedman Ellis <greg@crunch.io>
2020-12-14 12:41:21 -0600

    Use vdiffr conditional on it being installed

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,9 @@
 # This is here instead of in testthat because `manage_cases` doesn't
 # seem to load testthat.R
-library(vdiffr)
+expect_doppelganger <- function(title, fig, path = NULL, ...) {
+    testthat::skip_if_not_installed("vdiffr")
+    vdiffr::expect_doppelganger(title, fig, path = path, ...)
+}
 
 Sys.setlocale("LC_COLLATE", "C") ## What CRAN does; affects sort order
 set.seed(999) ## To ensure that tests that involve randomness are reproducible
